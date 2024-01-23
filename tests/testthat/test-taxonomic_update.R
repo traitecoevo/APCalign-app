6c8feb8c1e4cdbfae58c65e7555ace5b51e7b5b7
Fonti Kar <f.kar@unsw.edu.au>
2024-01-24 07:25:49 +1000

    Updated tests

--- tests/testthat/test-taxonomic_update.R
+++ tests/testthat/test-taxonomic_update.R
@@ -1,13 +1,33 @@
-test_that("names and output updates", {
+test_that("reactive executes", {
   testServer(server_test, {
     session$setInputs(submit_button = 1,
                       names_input = "Acacia aneura",
-                      full = FALSE,
+                      full = TRUE,
                       taxonomic_splits = "most_likely_species")
     
     expect_named(dataInput())
     expect_s3_class(dataInput(), "data.frame")
+    expect_equal(ncol(dataInput()), 21)
+    expect_equal(nrow(dataInput()), 1)
   })
 })
+
+
+test_that("Output changes with inputs", {
+  testServer(server_test, {
+    session$setInputs(submit_button = 1,
+                      names_input = "Acacia aneura",
+                      full = FALSE,
+                      taxonomic_splits = "return_all")
+    
+    expect_named(dataInput())
+    expect_s3_class(dataInput(), "data.frame")
+    expect_equal(ncol(dataInput()), 12)
+    expect_equal(nrow(dataInput()), 3)
+  })
+})
+
+
+
 
 
